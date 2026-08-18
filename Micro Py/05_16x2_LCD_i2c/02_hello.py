e7fa1dcdcaec5abe186af39ea95cec6f340f2d71
Rajsekhar Panda <132293033+panda-rajsekhar@users.noreply.github.com>
2026-08-19 01:16:59 +0530

    Refactor I2C setup and LCD initialization

--- Micro Py/05_16x2_LCD_i2c/02_hello.py
+++ Micro Py/05_16x2_LCD_i2c/02_hello.py
@@ -1,14 +1,17 @@
-from machine import Pin, I2C
+from machine import I2C, Pin
 from i2c_lcd import I2cLcd
 
+I2C_ID = 0
+SDA_PIN = 0
+SCL_PIN = 1
+I2C_ADDRESS = 0x27
+
 i2c = I2C(
-    0,
-    sda=Pin(0),
-    scl=Pin(1),
-    freq=400000
+    I2C_ID,
+    sda=Pin(SDA_PIN),
+    scl=Pin(SCL_PIN),
+    freq=400_000,
 )
 
-lcd = I2cLcd(i2c, 0x27, 2, 16)
-
+lcd = I2cLcd(i2c, I2C_ADDRESS, rows=2, cols=16)
 lcd.putstr("Hello World!")
-
